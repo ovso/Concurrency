f1c5e0a50f18cd69a7d645291d088a7e1b59e4f3
jaeho <ovsoce@gmail.com>
2020-08-04 08:08:30 +0900

    feat: Create newSingleThreadContext

--- app/src/main/java/io/github/ovso/concurrency/MainActivity.kt
+++ app/src/main/java/io/github/ovso/concurrency/MainActivity.kt
@@ -2,8 +2,10 @@ package io.github.ovso.concurrency
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.newSingleThreadContext
 
 class MainActivity : AppCompatActivity() {
+    val netDispatcher = newSingleThreadContext(name = "ServiceCall")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
