9029d561ea7aa662bbcd1ef2d51f48c2939b5288
jaeho <ovsoce@gmail.com>
2020-08-06 07:38:23 +0900

    feat: MainActivity.kt에서 코루틴 시작시 쓰레드 지정

--- app/src/main/java/io/github/ovso/concurrency/MainActivity.kt
+++ app/src/main/java/io/github/ovso/concurrency/MainActivity.kt
@@ -2,12 +2,17 @@ package io.github.ovso.concurrency
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
 
 class MainActivity : AppCompatActivity() {
-    val netDispatcher = newSingleThreadContext(name = "ServiceCall")
+    val dispatcher = newSingleThreadContext(name = "ServiceCall")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        GlobalScope.launch(dispatcher) {
+
+        }
     }
 }
