3f00b562aa726460b0161ad62b57dffd3adbb1c4
jaeho <ovsoce@gmail.com>
2020-08-06 08:24:48 +0900

    feat: RSS 가져오기(계속)

--- app/src/main/java/io/github/ovso/concurrency/MainActivity.kt
+++ app/src/main/java/io/github/ovso/concurrency/MainActivity.kt
@@ -5,14 +5,36 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : AppCompatActivity() {
-    val dispatcher = newSingleThreadContext(name = "ServiceCall")
+    private val dispatcher = newSingleThreadContext(name = "ServiceCall")
+    private val factory = DocumentBuilderFactory.newInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         GlobalScope.launch(dispatcher) {
-
+            val headlines = fetchRssHeadlines()
+            println(headlines)
         }
+    }
+
+    private fun fetchRssHeadlines(): List<String> {
+        val builder = factory.newDocumentBuilder()
+        val xml = builder.parse("https://ovso.github.io/feed.xml")
+        val entries = xml.getElementsByTagName("entry")
+        val news = xml.getElementsByTagName("entry").item(0)
+        return (0 until news.childNodes.length)
+            .asSequence()
+            .map { news.childNodes.item(it) }
+            .filter { Node.ELEMENT_NODE == it.nodeType }
+            .map { it as Element }
+            .filter { "item" == it.tagName }
+            .map {
+                it.getElementsByTagName("title").item(0).textContent
+            }
+            .toList()
     }
 }
