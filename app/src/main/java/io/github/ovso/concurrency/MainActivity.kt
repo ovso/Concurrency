f12655626b649536a1ecf054e30c11c3212a306f
jaeho <ovsoce@gmail.com>
2020-08-07 07:51:32 +0900

    feat: Display news count

--- app/src/main/java/io/github/ovso/concurrency/MainActivity.kt
+++ app/src/main/java/io/github/ovso/concurrency/MainActivity.kt
@@ -3,6 +3,7 @@ package io.github.ovso.concurrency
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
@@ -16,7 +17,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         GlobalScope.launch(dispatcher) {
             val headlines = fetchRssHeadlines()
-            newsCount.text = "Found ${headlines.count()} News"
+            GlobalScope.launch(Dispatchers.Main) {
+                newsCount.text = "Found ${headlines.count()} News"
+            }
             println(headlines)
         }
     }
